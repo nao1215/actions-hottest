ad62d596a58b6f2e9e3012a3b10a624d9b70fcc9
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-12-01 01:53:43 +0900

    Check exit status code

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,3 +5,11 @@ go install github.com/nao1215/hottest@latest
 
 # Run hottest with user-specified arguments
 hottest "$@"
+
+# Check the exit status of hottest
+hottest_exit_status=$?
+
+# If hottest exit status is not 0, set the script exit status to 1 (failure)
+if [ $hottest_exit_status -ne 0 ]; then
+    exit 1
+fi
