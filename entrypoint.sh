029f242d7c3baa2e79b1a1741db06748bab2b2be
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2023-12-01 01:57:04 +0900

    print exit status

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,7 @@ hottest "$@"
 
 # Check the exit status of hottest
 hottest_exit_status=$?
+echo "exit status: $hottest_exit_status"
 
 # If hottest exit status is not 0, set the script exit status to 1 (failure)
 if [ $hottest_exit_status -ne 0 ]; then
